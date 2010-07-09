6e0f649f5bf37bc0713014b748a7c4176ddd6cd0
GordonDiggs <gordon@gordondiggs.com>
2010-07-09 15:44:01 -0400

    Let's hope it's right this time

--- lib/contacts/outlook.rb
+++ lib/contacts/outlook.rb
@@ -6,7 +6,7 @@ class Contacts
     def initialize(file)
       @contact_file = Array.new
       file.each do |line|
-        @contact_file << FasterCSV.parse(line)
+        @contact_file << FasterCSV.parse(line)[0]
       end
       @full_name = false
       @header_indexes = Hash.new
